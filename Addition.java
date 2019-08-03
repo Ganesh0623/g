c240cd26101fb0aab85547819e798a652db57d1b
ganesh <b@gmail.com>
2019-08-03 17:45:52 -0500

    Adding 2 files

---
+++ Addition.java
@@ -0,0 +1,27 @@
+public class Addition
+{
+	 
+	public static void main(String[] args) 
+	{
+
+		int [] i= new int[4];
+		i[0]=1;
+		i[2]=2;
+		i[3]=3;
+		
+		 int sum =0;
+
+		 sum = (i[0]+i[1]+i[2]+i[3]);
+		System.out.println(sum);
+
+for(int i1     : i)
+		{
+ sum += i1;
+System.out.println(sum);
+}
+
+
+
+
+	}
+}
